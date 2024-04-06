cda65e1b3d5ff9728fa8be2b5475e078343f7209
YoungGoofy <vbi4enkov@yandex.ru>
2024-04-06 18:21:33 +0300

    add handlers for struct "Scan"

--- pkg/gozap/scanner.go
+++ pkg/gozap/scanner.go
@@ -8,3 +8,11 @@ type Scan struct {
 func NewScan(url, apiKey string) *Scan {
 	return &Scan{url: url, apiKey: apiKey}
 }
+
+func (s *Scan) AddUrl(url string) {
+	s.url = url
+}
+
+func (s *Scan) AddApiKey(apiKey string) {
+	s.apiKey = apiKey
+}
